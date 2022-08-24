6046a56ed0b1af2badec4447d1f46efda191e379
shuyin21 <shuyindev@gmail.com>
2022-08-24 18:10:28 +0100

    first commit

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import FeedbackItem from './FeedbackItem'
+
+const FeedbackList = ({ feedback, handleDelete }) => {
+
+    if (!feedback || feedback.length === 0) {
+        return <p>No feedback yet</p>
+    }
+
+    return (
+        <div className='feedback-list'>
+            {feedback.map((item) => (
+                <FeedbackItem key={item.id} item={item}
+                    handleDelete={handleDelete}
+                />
+            ))}
+        </div>
+    )
+}
+FeedbackList.propTypes = {
+    feedback: PropTypes.array,
+}
+
+
+export default FeedbackList
