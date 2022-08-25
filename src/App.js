05a60b00c55ed9176b0076e97927225929e50e82
shuyin21 <shuyindev@gmail.com>
2022-08-25 18:47:01 +0100

    animation added to list

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
@@ -18,12 +19,16 @@ function App() {
 
 
   }
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
 
+  }
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={
           deleteFeedback
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -7,7 +7,7 @@ import RatingSelect from './RatingSelect';
 
 
 
-const FeedbackForm = () => {
+const FeedbackForm = ({ handleAdd }) => {
     const [text, setText] = useState('')
     const [rating, setRating] = useState(10)
     const [btnDisabled, setBtnDisabled] = useState(true)
@@ -29,10 +29,21 @@ const FeedbackForm = () => {
         setText(e.target.value);
 
     }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating,
 
+            }
+            handleAdd(newFeedback);
+            setText('')
+        }
+    }
     return (
         <Card>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate your service with us?</h2>
                 <RatingSelect select={(rating) => { setRating(rating); }} />
                 <div className="input-group">
